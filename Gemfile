c3181eb21bf9fb6aceedb0eccab487c48b5b3d13
andrii.suboch <andrii.suboch@cd.p2h.com>
2024-08-05 11:48:43 +0300

    [BP-1]: create users and posts tables

--- Gemfile
+++ Gemfile
@@ -40,3 +40,8 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+gem "devise", "~> 4.9"
+
+# Access rights for users
+gem 'pundit', '~> 2.1.0'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
-class ApplicationController < ActionController::API
+# frozen_string_literal: true
+
+class ApplicationController < ActionController::Base
+  include Pundit
+
+  # Pundit: rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_to(request.referrer || root_path)
+  end
 end
